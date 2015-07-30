5b841e89e29594179fcdae9ac177eb950f94cb19
Lily Bickerstaffe <lily@debian>
2015-07-30 17:28:20 +0100

    connected crawler to SQL database and added functionality to process all the pages in the list

--- crawler.rb
+++ crawler.rb
@@ -2,36 +2,108 @@ require 'json'
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'mysql'
 
 # Fetching the page
-source_url = 'http://www.radiolab.org/blogs/radiolab-blogland/'
-page = Nokogiri::HTML(open(source_url))
+base_url = 'http://www.radiolab.org/blogs/radiolab-blogland/'
+page_url = ''
 
-# get article details of first in list (class="channel-item first")
-title_first = page.css('div[class="channel-item first"]').css('h2').text
-date_first = page.css('div[class="channel-item first"]').css('h3').text
-tease_first = page.css('div[class="channel-item first"]').css('div[class="no-object"]').text
-## author_first
+nextpage_exists = true
+page_number = 1
 
-# get article details of the rest (class="channel-item")
-articles = page.css('div[class="channel-item"]')
+# connecting to mysql
+begin
+    con = Mysql.new 'localhost', 'XXXXXXX', 'XXXXXXX'
 
-articles.each do |article|
-	title = article.css('h2').text.strip!
-	date = article.css('h3').text.strip!
-	tease = article.css('div[class="no-object"]').text.strip!
+	while nextpage_exists
 
-	# check if there is an author for the article
-	if article.css('p[class="byline"]').any?
-		author = article.css('p[class="byline"]').css('a').text
-	else
-		author = 'unknown'
+		page = Nokogiri::HTML(open(base_url+page_url))
+
+		# first article in list has its own html class ("channel-item first"))
+		# get article details of first in list
+		title = page.css('div[class="channel-item first"]').css('h2').text.strip!
+		date = page.css('div[class="channel-item first"]').css('h3').text.strip!
+		tease = page.css('div[class="channel-item first"]').css('div[class="no-object"]').text.strip!
+		source = 'radiolab'
+
+		# check if there is an author for the first article
+		if page.css('div[class="channel-item first"]').css('p[class="byline"]').any?
+			author = page.css('div[class="channel-item first"]').css('p[class="byline"]').css('a').text
+		else
+			author = 'unknown'
+		end
+
+		# replace ' with '' in strings to enable sql query
+		title.gsub! "'", "''"
+		tease.gsub! "'", "''"
+		#author.sub! "'", "''"
+
+		# remove &nbsp; from string
+		#title.sub! "&nbsp;", " "
+		#tease.sub! "&nbsp;", " "
+		#author.sub! "&nbsp;", " "
+
+		# print out result
+		# puts title
+
+		# update sql database
+		query = con.query "INSERT INTO  `simple_crawler`.`Article_Summary` (`ID`,`Title`,`Article_Date`,`Author`,`Tease`,`Source`,`Updated`)
+				VALUES (NULL,'#{title}','#{date}','#{author}','#{tease}','#{source}',CURRENT_TIMESTAMP)
+				ON DUPLICATE KEY UPDATE Tease='#{tease}', Updated=CURRENT_TIMESTAMP"
+
+		# get article details for the rest (class="channel-item")
+		articles = page.css('div[class="channel-item"]')
+
+		articles.each do |article|
+			title = article.css('h2').text.strip!
+			date = article.css('h3').text.strip!
+			tease = article.css('div[class="no-object"]').text.strip!
+			source = 'radiolab'
+
+			# check if there is an author for the article
+			if article.css('p[class="byline"]').any?
+				author = article.css('p[class="byline"]').css('a').text
+				puts author
+			else
+				author = 'unknown'
+			end
+			
+			# replace ' with '' in fields to enable sql query
+			title.gsub! "'", "''"
+			tease.gsub! "'", "''"
+			
+			# remove &nbsp; from fields
+			#title.gsub! "&nbsp;", " "
+			#tease.gsub! "&nbsp;", " "
+
+			puts title
+			
+			# update sql database
+			query = con.query "INSERT INTO  `simple_crawler`.`Article_Summary` (`ID`,`Title`,`Article_Date`,`Author`,`Tease`,`Source`,`Updated`)
+					VALUES (NULL,'#{title}','#{date}','#{author}','#{tease}','#{source}',CURRENT_TIMESTAMP)
+					ON DUPLICATE KEY UPDATE Tease='#{tease}', Updated=CURRENT_TIMESTAMP"
+
+			# print our results
+			# puts title
+			#puts date
+			#puts tease
+			#puts author
+			#puts '---------------------------------------------'
+		end
+
+		page_number += 1
+		page_url = page_number.to_s + '/'
+
+		nextpage_exists = page.css('span[class="pagefooter-next"]').any?
+
 	end
 
-	# print our results
-	puts title
-	puts date
-	puts tease
-	puts author
-	puts '---------------------------------------------'
+# catch errors with sql database
+rescue Mysql::Error => e
+    puts e.errno
+    puts e.error
+
+# close mysql connection
+ensure
+    con.close if con
 end
