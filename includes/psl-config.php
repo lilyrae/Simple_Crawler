22208d98522eea024671418e6348ba47eaf52d4e
Lily Bickerstaffe <lily@debian>
2015-07-31 16:26:59 +0100

    updated database

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -3,8 +3,8 @@
  * These are the database login details
  */  
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "root");    // The database username. 
-define("PASSWORD", "huey123");    // The database password. 
+define("USER", "XXXXXX");    // The database username. 
+define("PASSWORD", "XXXXXX");    // The database password. 
 define("DATABASE", "secure_login");    // The database name.
  
 define("CAN_REGISTER", "any");
